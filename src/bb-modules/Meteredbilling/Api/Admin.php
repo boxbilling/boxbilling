8ae39e6c573fd641c19df2c478f504d98af4455d
Vaidas Lungis <vaidas.lungis+github@gmail.com>
2014-12-08 15:57:19 +0200

    add meteredbilling\get_total_cost() api call

---
+++ src/bb-modules/Meteredbilling/Api/Admin.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * BoxBilling
+ *
+ * @copyright BoxBilling, Inc (http://www.boxbilling.com)
+ * @license   Apache-2.0
+ *
+ * Copyright BoxBilling, Inc
+ * This source file is subject to the Apache-2.0 License that is bundled
+ * with this source code in the file LICENSE
+ */
+
+namespace Box\Mod\Meteredbilling\Api;
+
+class Admin extends \Api_Abstract
+{
+
+    /**
+     * Returns total amount of unpaid order usage
+     * @param $data - order_id
+     * @return bool
+     */
+    public function get_total_cost($data)
+    {
+        $required = array(
+            'order_id' => 'Missing order id',
+        );
+        $this->di['validator']->checkRequiredParamsForArray($required, $data);
+
+        $orderModel = $this->di['db']->getExistingModelById('ClientOrder', $data['order_id'], 'Order not found');
+        return $this->getService()->getOrderUsageTotalCost($orderModel);
+    }
+}
---
+++ tests/bb-modules/Meteredbilling/Api/Api_AdminTest.php
@@ -0,0 +1,55 @@
+<?php
+
+class AdminTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \Box\Mod\Meteredbilling\Api\Admin
+     */
+    protected $api = null;
+
+    public function setup()
+    {
+        $this->api = new \Box\Mod\Meteredbilling\Api\Admin();
+    }
+
+    public function testgetDi()
+    {
+        $di = new \Box_Di();
+        $this->api->setDi($di);
+        $getDi = $this->api->getDi();
+        $this->assertEquals($di, $getDi);
+    }
+
+    public function testget_total_cost()
+    {
+        $validatorMock = $this->getMockBuilder('\Box_Validate')->disableOriginalConstructor()->getMock();
+        $validatorMock->expects($this->atLeastOnce())
+            ->method('checkRequiredParamsForArray');
+
+        $model = new Model_ClientOrder();
+        $model->loadBean(new \RedBeanPHP\OODBBean());
+
+        $dbMock = $this->getMockBuilder('\Box_Database')->getMock();
+        $dbMock->expects($this->atLeastOnce())
+            ->method('getExistingModelById')
+            ->with('ClientOrder')
+            ->willReturn($model);
+
+        $di = new Box_Di();
+        $di['db'] = $dbMock;
+        $di['validator'] = $validatorMock;
+        $this->api->setDi($di);
+
+        $serviceMock = $this->getMockBuilder('\Box\Mod\Meteredbilling\Service')->getMock();
+        $serviceMock->expects($this->atLeastOnce())
+            ->method('getOrderUsageTotalCost')
+            ->willReturn(0.000001);
+        $this->api->setService($serviceMock);
+
+        $data = array('order_id' => 1);
+        $result = $this->api->get_total_cost($data);
+        $this->assertGreaterThan(0.00000000, $result);
+    }
+}
+ 
