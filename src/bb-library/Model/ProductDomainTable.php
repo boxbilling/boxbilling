579030bcd2bce57f03b4ae01f838b7b8445e69df
fordnox <fordnox@gmail.com>
2020-03-07 18:11:25 +0200

    Fix

--- src/bb-library/Model/ProductDomainTable.php
+++ src/bb-library/Model/ProductDomainTable.php
@@ -125,7 +125,7 @@ class Model_ProductDomainTable extends Model_ProductTable
         return $pricing;
     }
 
-    public function getProductPrice(Model_Product $product, array $config = array())
+    public function getProductPrice(Model_Product $product, array $config = null)
     {
         $rtable = $this->di['mod_service']('servicedomain', 'Tld');
         $tld = '';
@@ -162,7 +162,7 @@ class Model_ProductDomainTable extends Model_ProductTable
         return 0;
     }
 
-    public function getProductSetupPrice(Model_Product $product, array $config = array())
+    public function getProductSetupPrice(Model_Product $product, array $config = null)
     {
         return 0;
     }
