9f29a7a234e1a94337db1a33c9e5efe7a30545a9
Timothy Webb <webb.timothy@gmail.com>
2021-08-15 21:34:17 -0400

    Updated bb-load.php

--- src/bb-load.php
+++ src/bb-load.php
@@ -257,7 +257,9 @@ if(file_exists($configPath) && 0 !== filesize($configPath) && file_exists(BB_PAT
 
 $config = require_once $configPath;
 require BB_PATH_VENDOR . '/autoload.php';
-
+include_once BB_PATH_VENDOR .'/owasp/csrf-protector-php/libs/csrf/csrfprotector.php';
+// Initialize CSRFProtector library
+csrfProtector::init();
 date_default_timezone_set($config['timezone']);
 
 define('BB_DEBUG',          $config['debug']);
@@ -273,10 +275,8 @@ if($config['sef_urls']) {
     define('BB_URL_API',    $config['url'] . 'index.php?_url=/api/');
 }
 
-include_once BB_PATH_VENDOR .'/owasp/csrf-protector-php/libs/csrf/csrfprotector.php';';
 
-// Initialize CSRFProtector library
-csrfProtector::init();
+
 
 if($config['debug']) {
     error_reporting(E_ALL);
