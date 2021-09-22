b7fa0b04b56a9a3ea95938f0a5de0a04d2af6e4d
Yağızhan <yagizhan49@protonmail.com>
2021-09-22 17:13:52 +0000

    Creating box.js, the website will now fetch the latest release from GitHub

---
+++ assets/js/box.js
@@ -0,0 +1,38 @@
+/**
+ * BoxBilling
+ *
+ * @copyright BoxBilling (https://boxbilling.org)
+ * @license   Apache-2.0
+ *
+ * Copyright BoxBilling
+ * This source file is subject to the Apache-2.0 License that is bundled
+ * with this source code in the file LICENSE
+ */
+
+function getLatestReleaseVersion() {
+    // Let's see if the browser supports fetch()
+    if (window.fetch) {
+        // It does, now let's fetch from GitHub releases
+        fetch('https://api.github.com/repos/boxbilling/boxbilling/releases/latest')
+        .then(response => response.json())
+        .then(data => {
+            // Using release data
+            document.getElementById('latest-release-date').innerHTML = `Latest release: ${new Date(data.published_at).toLocaleDateString()}`;
+            document.getElementById('download-text').innerHTML = data.tag_name;
+            document.getElementById('navbar-download').setAttribute('href', data.html_url);
+            document.getElementById('primary-download').setAttribute('href', data.html_url);
+        }).catch(error => {
+            // Uh oh
+            console.error('There has been a problem while fetching versioning details from GitHub releases:', error);
+        });
+      } else {
+        // It doesn't
+        console.error('Your browser does not support fetching from another source. Details about the latest release were not fetched from GitHub, showing dummy values instead.')
+      }
+}
+
+function loader() {
+    getLatestReleaseVersion();
+}
+
+window.onload = loader;
